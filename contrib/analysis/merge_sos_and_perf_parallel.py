ec88d76b9e68e81b6edfbfd365d14d39ee6c3988
Rohit Mohnani <rmohnani@redhat.com>
2022-07-01 20:44:30 -0400

    Delete old code and Add documentation for merge_sos_and_perf_parallel.py functions

--- contrib/analysis/merge_sos_and_perf_parallel.py
+++ contrib/analysis/merge_sos_and_perf_parallel.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
 import multiprocessing
-import os
 import requests
-import sys
 import time
 
-from collections import OrderedDict
 from datetime import datetime
 from dateutil import rrule
 from dateutil.relativedelta import relativedelta
-from pbench_combined_data import PbenchCombinedData, PbenchCombinedDataCollection
+from pbench_combined_data import PbenchCombinedDataCollection
 
 from elasticsearch1 import Elasticsearch
 from elasticsearch1.helpers import scan
@@ -28,29 +24,42 @@ def _month_gen(now: datetime):
     for m in rrule.rrule(rrule.MONTHLY, dtstart=first_month, until=last_month):
         yield f"{m.year:04}-{m.month:02}"
 
+def merge_run_result_index(es : Elasticsearch, month : str, record_limit : int, pbench_data : PbenchCombinedDataCollection) -> None:
+    """Merges all run and result data for a given month and stores it inside pbench_data.
 
-def result_index_gen(month_gen):
-    """Yield all the result index patterns for each month yielded from the given
-    generator.
+    Given a month, gets the run_index and result_index names for the month specified.
+    Loops over every doc in the run_index that is of type 'pbench-run', and
+    adds it to pbench_data. Checks if valid record_limit is met and stops
+    going through more run data. Then loops over all result docs in the month
+    of type 'pbench-result-data-sample' and adds it to pbench_data.
+
+    #NOTE: Need to still go through all result data for the month to ensure we 
+           retrive all result data associated with the runs added, since we
+           don't know more specifically the associations within the index, this
+           is the best we can do so far.
+
+    #NOTE: Since only within months do the processing of run and result need to
+           be sequential, we can process multiple months in parallel,
+           hopefully reducing time taken overall.
+
+    Parameters
+    ----------
+    es : Elasticsearch
+        Elasticsearch object where data is stored
+    month : str
+        Month Year string stored in YYYY-MM format
+    record_limit : int
+        Number of valid run records desired.
+        Serves as termination check.
+    pbench_data : PbenchCombinedDataCollection
+        Store for all the processed pbench data and diagnostic
+        and tracking information.
+    
+    Returns
+    -------
+    None
+
     """
-    for month in month_gen:
-        yield f"dsa-pbench.v4.result-data.{month}-*"
-
-
-def run_index_gen(month_gen):
-    """Yield all the run data index patterns for each month yielded from the given
-    generator.
-    """
-    for month in month_gen:
-        yield f"dsa-pbench.v4.run.{month}"
-
-def paired_run_result_index_gen(month_gen):
-    for month in month_gen:
-        run_index = f"dsa-pbench.v4.run.{month}"
-        result_index = f"dsa-pbench.v4.result-data.{month}-*"
-        # yield run_index, result_index
-
-def merge_run_result_index(es, month, record_limit, pbench_data : PbenchCombinedDataCollection):
     run_index = f"dsa-pbench.v4.run.{month}"
     result_index = f"dsa-pbench.v4.result-data.{month}-*"
 
@@ -59,19 +68,29 @@ def merge_run_result_index(es, month, record_limit, pbench_data : PbenchCombined
         if record_limit != -1:
             if pbench_data.trackers["run"]["valid"] >= record_limit:
                 break
-    # print("finish run data processing")
+
     for result_doc in es_data_gen(es, result_index, "pbench-result-data-sample"):
         pbench_data.add_result(result_doc)
-        # print("processed a result")
-    
-    return pbench_data
-
-    # for doc in es_data_gen(es, result_index, "pbench-result-data-sample"):
 
 
-def es_data_gen(es, index, doc_type):
+def es_data_gen(es : Elasticsearch, index : str, doc_type : str):
     """Yield documents where the `run.script` field is "fio" for the given index
     and document type.
+
+    Parameters
+    ----------
+    es : Elasticsearch
+        Elasticsearch object where data is stored
+    index : str
+        index name
+    doc_type : str
+        document type
+
+    Yields
+    -------
+    doc : json
+        json data representing doc and its contents
+    
     """
     query = {"query": {"query_string": {"query": "run.script:fio"}}}
 
@@ -84,304 +103,6 @@ def es_data_gen(es, index, doc_type):
         request_timeout=3600,  # to prevent timeout errors (3600 is arbitrary)
     ):
         yield doc
-
-
-def pbench_runs_gen(es, month_gen):
-    """Yield all the pbench run documents using the given month generator."""
-    for run_index in run_index_gen(month_gen):
-        for doc in es_data_gen(es, run_index, "pbench-run"):
-            yield doc
-
-
-def pbench_result_data_samples_gen(es, month_gen):
-    """Yield all the pbench result data sample documents using the given month
-    generator.
-    """
-    for result_index in result_index_gen(month_gen):
-        for doc in es_data_gen(es, result_index, "pbench-result-data-sample"):
-            yield doc
-
-def load_pbench_runs(es, now: datetime, record_limit):
-    """Load all the pbench run data, sub-setting to contain only the fields we require.
-    We also ignore any pbench run without a `controller_dir` field or without
-    a `sosreports` field. A few statistics about the processing is printed to stdout.
-    Returns a dictionary containing the processed pbench run documents
-    """
-    pbench_data = PbenchCombinedDataCollection()
-    
-    for _source in pbench_runs_gen(es, _month_gen(now)):
-        pbench_data.add_run(_source)
-        
-        if pbench_data.trackers["run"]["valid"] >= record_limit:
-            break
-    
-    pbench_data.print_stats()
-    # return pbench_data.get_runs()
-    return pbench_data
-
-# extract list of clients from the URL
-def extract_clients(results_meta, es):
-    run_index = results_meta["run_index"]
-    parent_id = results_meta["run.id"]
-    iter_name = results_meta["iteration.name"]
-    sample_name = results_meta["sample.name"]
-    parent_dir_name = f"/{iter_name}/{sample_name}/clients"
-    query = {
-        "query": {
-            "query_string": {
-                "query": f'_parent:"{parent_id}"'
-                f' AND ancestor_path_elements:"{iter_name}"'
-                f' AND ancestor_path_elements:"{sample_name}"'
-                f" AND ancestor_path_elements:clients"
-            }
-        }
-    }
-
-    client_names_raw = []
-    for doc in scan(
-        es,
-        query=query,
-        index=run_index,
-        doc_type="pbench-run-toc-entry",
-        scroll="1m",
-        request_timeout=3600,  # to prevent timeout errors (3600 is arbitrary)
-    ):
-        src = doc["_source"]
-        if src["parent"] == parent_dir_name:
-            client_names_raw.append(src["name"])
-    # FIXME: if we have an empty list, do we still want to use those results?
-    return list(set(client_names_raw))
-
-
-# extract host and disk names from fio-result.txt
-def extract_fio_result(results_meta, incoming_url, session):
-    url = (
-        incoming_url
-        + results_meta["controller_dir"]
-        + "/"
-        + results_meta["run.name"]
-        + "/"
-        + results_meta["iteration.name"]
-        + "/"
-        + results_meta["sample.name"]
-        + "/"
-        + "fio-result.txt"
-    )
-
-    # check if the page is accessible
-    response = session.get(url, allow_redirects=True)
-    if response.status_code != 200:  # successful
-        # FIXME: are these results we still want?
-        return ([], [])
-
-    try:
-        document = response.json()
-    except ValueError:
-        # print("Response content is not valid JSON")
-        # print(url)
-        # print(response.content)
-        # FIXME: are these results we still want?
-        return ([], [])
-
-    try:
-        disk_util = document["disk_util"]
-    except KeyError:
-        disknames = []
-    else:
-        disknames = [disk["name"] for disk in disk_util if "name" in disk]
-
-    try:
-        client_stats = document["client_stats"]
-    except KeyError:
-        hostnames = []
-    else:
-        hostnames = list(
-            set([host["hostname"] for host in client_stats if "hostname" in host])
-        )
-
-    return (disknames, hostnames)
-
-
-def transform_result(source, pbench_runs, results_seen, stats):
-    """Transform the raw result data sample document to a stripped down version,
-    augmented with pbench run data.
-    """
-    # print(source)
-    # print("\n")
-    result_id = source["_id"]
-    assert result_id not in results_seen, f"Result ID {result_id} repeated"
-    results_seen[result_id] = True
-
-    try:
-        index = source["_source"]
-        run = index["run"]
-        run_id = run["id"]
-        run_name = run["name"]
-        iter_name = index["iteration"]["name"]
-        sample = index["sample"]
-        sample_name = sample["name"]
-        sample_m_type = sample["measurement_type"]
-        sample_m_title = sample["measurement_title"]
-        sample_m_idx = sample["measurement_idx"]
-    except KeyError as exc:
-        print(
-            # f"ERROR - {filename}, {exc}, {json.dumps(index)}", file=sys.stderr,
-            f"ERROR - {exc}, {json.dumps(index)}",
-            file=sys.stderr,
-        )
-        stats["errors"] += 1
-        return None
-
-    try:
-        pbench_run = pbench_runs[run_id]
-    except KeyError:
-        # print(
-        #    f"*** Result without a run: {run_id}/{run_name}/{iter_name}"
-        #    f"/{sample_name}/{sample_m_type}/{sample_m_title}"
-        #    f"/{sample_m_idx}",
-        #    flush=True,
-        # )
-        stats["missing_runs"] += 1
-        return None
-
-    if "mean" not in sample:
-        # run_ctrl = pbench_run["controller_dir"]
-        # print(
-        #    f"No 'mean' in {run_ctrl}/{run_name}/{iter_name}"
-        #    f"/{sample_name}/{sample_m_type}/{sample_m_title}"
-        #    f"/{sample_m_idx}",
-        #    flush=True,
-        # )
-        stats["missing_mean"] += 1
-        return None
-
-    if sample["client_hostname"] == "all":
-        stats["aggregate_result"] += 1
-        return None
-
-    # The following field names are required
-    try:
-        benchmark = index["benchmark"]
-        # why do we care about insertion order here?
-        result = OrderedDict()
-        result.update(
-            [
-                ("run.id", run_id),
-                ("iteration.name", iter_name),
-                ("sample.name", sample_name),
-                ("run.name", run_name),
-                ("benchmark.bs", benchmark["bs"]),
-                ("benchmark.direct", benchmark["direct"]),
-                ("benchmark.ioengine", benchmark["ioengine"]),
-                ("benchmark.max_stddevpct", benchmark["max_stddevpct"]),
-                ("benchmark.primary_metric", benchmark["primary_metric"]),
-                ("benchmark.rw", ", ".join(set((benchmark["rw"].split(","))))),
-                ("sample.client_hostname", sample["client_hostname"]),
-                ("sample.measurement_type", sample_m_type),
-                ("sample.measurement_title", sample_m_title),
-                ("sample.measurement_idx", sample_m_idx),
-                ("sample.mean", sample["mean"]),
-                ("sample.stddev", sample["stddev"]),
-                ("sample.stddevpct", sample["stddevpct"]),
-            ]
-        )
-    except KeyError as exc:
-        print(
-            # f"ERROR - {filename}, {exc}, {json.dumps(index)}", file=sys.stderr,
-            f"ERROR - {exc}, {json.dumps(index)}",
-            file=sys.stderr,
-        )
-        stats["errors"] += 1
-        return None
-
-    stats["mean"] += 1
-
-    result["run_index"] = pbench_run["run_index"]
-    result["controller_dir"] = pbench_run["controller_dir"]
-    result["sosreports"] = pbench_run["sosreports"]
-
-    # optional workload parameters accounting for defaults if not found
-
-    result["benchmark.filename"] = sentence_setify(
-        benchmark.get("filename", "/tmp/fio")
-    )
-    result["benchmark.iodepth"] = benchmark.get("iodepth", "32")
-    result["benchmark.size"] = sentence_setify(benchmark.get("size", "4096M"))
-    result["benchmark.numjobs"] = sentence_setify(benchmark.get("numjobs", "1"))
-    result["benchmark.ramp_time"] = benchmark.get("ramp_time", "none")
-    result["benchmark.runtime"] = benchmark.get("runtime", "none")
-    result["benchmark.sync"] = benchmark.get("sync", "none")
-    result["benchmark.time_based"] = benchmark.get("time_based", "none")
-
-    return result
-
-
-def sentence_setify(sentence: str) -> str:
-    """Splits input by ", " gets rid of duplicates and rejoins unique
-    items into original format. Effectively removes duplicates in input.
-    """
-    return ", ".join(set([word.strip() for word in sentence.split(",")]))
-
-
-def process_results(es, now, session, incoming_url, pool, pbench_runs, stats):
-    """Intermediate generator for handling the fetching of the client names, disk
-    names, and host names.
-
-    """
-    stats["total_recs"] = 0
-    stats["missing_mean"] = 0
-    stats["missing_runs"] = 0
-    stats["aggregate_result"] = 0
-    stats["multiple_clients"] = 0
-    stats["no_clients"] = 0
-    stats["errors"] = 0
-    stats["mean"] = 0
-
-    results_seen = dict()
-    clientnames_map = dict()
-    diskhost_map = dict()
-
-    for _source in pbench_result_data_samples_gen(es, _month_gen(now)):
-        # print("source" + _source)
-        # print("\n")
-        stats["total_recs"] += 1
-        result = transform_result(_source, pbench_runs, results_seen, stats)
-        if result is None:
-            continue
-
-        # Add host and disk names in the data here
-        key = f'{result["run.id"]}/{result["iteration.name"]}'
-        try:
-            disknames, hostnames = diskhost_map[key]
-        except KeyError:
-            print(f"{key} / fio-result.txt", flush=True)
-            disknames, hostnames = extract_fio_result(result, incoming_url, session)
-            diskhost_map[key] = (disknames, hostnames)
-        result["disknames"] = disknames
-        result["hostnames"] = hostnames
-
-        # Add client names here
-        key = result["run.id"]
-        try:
-            clientnames = clientnames_map[key]
-        except KeyError:
-            print(f"{key} / client names", flush=True)
-            clientnames = extract_clients(result, es)
-            clientnames_map[key] = clientnames
-
-        # Ignore result if 0 or more than 1 client names
-        if not clientnames:
-            stats["no_clients"] += 1
-            continue
-
-        if len(clientnames) > 1:
-            stats["multiple_clients"] += 1
-            continue
-
-        result["clientnames"] = clientnames
-
-        yield result
-        
 
 
 def main(args):
@@ -420,13 +141,9 @@ def main(args):
         if args.record_limit != -1:
             if len(pbench_data.run_id_to_data_valid) >= args.record_limit:
                 break
-        # if len(pbench_data.run_id_to_data_valid) >= args.record_limit:
-        #     break
-
-    # pbench_runs = load_pbench_runs(es, now, args.record_limit)
     
-    # result_cnt = 0
-    # stats = dict()
+    # NOTE: Not writing sosreports and results to files. Will work on this step
+    #       of sosreport processing, etc next.
 
     # with open("sosreport_fio.txt", "w") as log, open("pbench_fio.json", "w") as outfile:
     #     generator = process_results(
@@ -447,9 +164,6 @@ def main(args):
     duration = scan_end - scan_start
 
     print(pbench_data)
-    
-    # print(f"final number of records: {result_cnt:n}", flush=True)
-    # print(json.dumps(stats, indent=4), flush=True)
     print(f"--- merging run and result data took {duration:0.2f} seconds", flush=True)
 
     if memprof:
@@ -460,7 +174,19 @@ def main(args):
 
     return 0
 
-def parse_arguments():
+def parse_arguments() -> argparse.Namespace:
+    """Specifies Command Line argument parsing.
+
+    Gives help info when running this file as to what arguments needed, etc.
+    Adds optional flags to change execution of code.
+
+    Returns
+    -------
+    args : argparse.Namespace
+        arguments passed in stored with easily accessible
+        variable names
+    
+    """
     parser = argparse.ArgumentParser(description= "Host and Server Information")
     parser.add_argument("es_host", action="store", type=str, help="Elasticsearch host name")
     parser.add_argument("es_port", action="store", type=int, help="Elasticsearch port number")
@@ -469,6 +195,7 @@ def parse_arguments():
     parser.add_argument("--cpu", action='store', dest="cpu_n", type=int, default=1, help="Number of CPUs to be used")
     parser.add_argument("--limit", action='store', dest="record_limit", type=int, default=-1, help="Number of desired acceptable results for processing")
     # TODO: need to figure out how to allow both limiting and non-limiting options with argparse. But suppose would never want to limit in real use case.
+    # Temporarily used -1 as default and it meaning all
     parser.add_argument("--profile", action='store_true', dest="profile_memory_usage", help="Want memory usage profile")
     args = parser.parse_args()
     return args
